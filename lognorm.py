a6d0081f025b18a7ce52769d186ca247de113621
lolme <kai.eba_to11@outlook.jp>
2022-12-05 02:00:35 +0900

    12-05

--- lognorm.py
+++ lognorm.py
@@ -1,4 +1,25 @@
+#データ元
+#https://ganjoho.jp/reg_stat/statistics/data/dl/index.html
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
-input = pd.ExcelFile("cancer.xlsx")
+input = pd.read_csv('./cancer.csv',header=None)
+#がん死亡者数[年度、男性の死亡者数、女性の死亡者数]
+cancers=input.values
+#年度
+year=[]
+#男性の死亡者数
+male=[]
+#女性の死亡者数
+female=[]
+
+for i in range(len(cancers)):
+    year.append(cancers[i][0])
+    male.append(cancers[i][1])
+    female.append(cancers[i][2])
+plt.xlim(year)
+plt.hist(male ,bins=63, align='mid')
+# plt.hist(female, align='mid')
+# plt.hist(year, align='mid')
+plt.show()
