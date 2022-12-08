a266edacc0c5b274e9cbae0f9db9c08b1d20a6bd
lolme <kai.eba_to11@outlook.jp>
2022-12-09 00:08:21 +0900

    12-09

--- lognorm.py
+++ lognorm.py
@@ -3,6 +3,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.stats import lognorm
+import math
+
+def log_normal_distribution(i, mu, sigma):
+  # 対数正規分布
+  x = (1 / (np.sqrt(2 * np.pi) * sigma * i))
+  y = (np.e ** (- ((math.log(i) - mu) ** 2) / (2 * sigma ** 2)))
+  result = x * y
+
+  return result
 
 input = pd.read_csv('./cancer.csv',header=None)
 #がん死亡者数[年度、男性の死亡者数、女性の死亡者数]
@@ -33,4 +43,27 @@ mu=np.log(mean/np.sqrt(1+var/(mean**2)))
 std=np.log(1+np.var(male)/(np.mean(male)**2))
 sigma=np.sqrt(std)
 
+# 対数正規分布を実行
+n = [log_normal_distribution(i, mu, sigma) for i in year]
+plt.plot(year, n)
+plt.show()
+
+plt.xlabel("Year", fontsize=20)  
+plt.ylabel("Fatalities", fontsize=20) 
+plt.bar(year, female, width=0.6)
+plt.show()
+#平均値
+mean=np.mean(female)
+#分散値
+var=np.var(female)
+#対数平均
+mu=np.log(mean/np.sqrt(1+var/(mean**2)))
+#対数標準偏差
+std=np.log(1+np.var(female)/(np.mean(female)**2))
+sigma=np.sqrt(std)
+
+# 対数正規分布を実行
+n = [log_normal_distribution(i, mu, sigma) for i in year]
+plt.plot(year, n)
+plt.show()
 
